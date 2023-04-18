ee4d41e94b178e26ae3797acb0b2701afb99dcd9
tztechno <47345279+tztechno@users.noreply.github.com>
2023-04-19 01:34:29 +0900

    Update ABC297_D.py

--- ABC297_D.py
+++ ABC297_D.py
@@ -1,4 +1,18 @@
-
+＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃
+#TLE
+a, b = map(int, input().split())
+t = 0
+while a != b:
+    if a > b:
+        a, b = b, a
+    if a < b:
+    	if a == 1:
+          t+= b - a
+          b = a
+    	if a > 1:
+          t+= b // a
+          b = b % a
+print(t)
 ＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃
 #TLE
 a, b = map(int, input().split())
