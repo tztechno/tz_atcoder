cff25e239fdf3f22fbc457703303f90374759380
tztechno <47345279+tztechno@users.noreply.github.com>
2023-04-19 00:47:21 +0900

    Update ABC297_D.py

--- ABC297_D.py
+++ ABC297_D.py
@@ -1,18 +1,24 @@
 ＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃
-
 #TLE
-
+a, b = map(int, input().split())
+t = 0 
+while a != b:
+    if a > b:
+        a, b = b, a
+    if a < b:
+        b = b - a
+        t += 1
+print(t)
+＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃
+#TLE
 a, b = map(int, input().split())
 t = 0
- 
 while a != b:
     if a > b:
         a = a - b
         t += 1
     elif a < b:
         b = b - a
-        t += 1
- 
+        t += 1 
 print(t)
-
 ＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃＃
