9c7f699e1fce80c21e52d2dc1ec4d1f70ad2d5dd
tztechno <47345279+tztechno@users.noreply.github.com>
2023-06-06 19:25:11 +0900

    Update ABC204_C.py

--- ABC204_C.py
+++ ABC204_C.py
@@ -1,4 +1,6 @@
 ##################
+#TLE
+
 n,D=map(int,input().split())
 XY=[]
 for i in range(n):
@@ -32,4 +34,39 @@ for i in range(n):
     print('Yes')
   else:
     print('No')
+    
+##################
+
+n,D=map(int,input().split())
+XY=[]
+for i in range(n):
+  x,y=map(int,input().split())
+  XY+=[(x,y)]
+#print(XY)
+E=[]
+for i in range(n):
+  E+=[set([i])]
+#print(E)
+for i in range(n):
+  for j in range(n):
+    if i!=j:
+      (x0,y0)=XY[i]
+      (x1,y1)=XY[j]
+      if (x1-x0)**2+(y1-y0)**2<=D**2:
+        E[i].add(j)
+        E[j].add(i)
+
+for i in range(n):
+  for j in range(n):
+    if E[i] & E[j]!=set():
+      E[i]=E[i]|E[j]
+      E[j]=E[i]|E[j]
+#print(E2)
+E3=sorted(E[0])
+for i in range(n):
+  if i in E3:
+    print('Yes')
+  else:
+    print('No')
+
 ##################
