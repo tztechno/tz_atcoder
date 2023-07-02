6810b9a2969dc65ad02f7ac911cc73c817a21bc2
tztechno <47345279+tztechno@users.noreply.github.com>
2023-07-02 19:57:54 +0900

    Update txt_input.py

--- txt_input.py
+++ txt_input.py
@@ -1,35 +1,28 @@
-with open('/kaggle/input/my-atcoder/in/0000.txt', 'r') as f:
-    data = f.read()
-lines = data.split('\n')
 
-XY=[]
-UVW=[]
-AB=[]
-for i,line in enumerate(lines):
+####################################
 
-    if i==0:
-        n,m,k=map(int,line.split())
-    elif 1<=i<1+n:
-        x,y=map(int,line.split())
-        XY+=[(x,y)]
-        if i==+n:
-            print(line)
-    elif 1+n<=i<1+n+m:
-        u,v,w=map(int,line.split())
-        UVW+=[(u,v,w)]
-        if i==+n+m:
-            print(line)        
-    elif 1+n+m<=i<1+n+m+k:
-        a,b=map(int,line.split())
-        AB+=[(a,b)]
-        if i==+n+m+k:
-            print(line)
-        
-print('n,m,k')
-print(n,m,k) 
-print('XY')
-print(XY[0:3])
-print('UVWQ')
-print(UVW[0:3])
-print('AB')
-print(AB[0:3])
+text = '''
+4 7
+3 13
+3 17
+5 29
+1 10'''
+
+lines = text.strip().split('\n')
+n, w = map(int, lines[0].split())
+WV = []
+
+for line in lines[1:]:
+    w, v = map(int, line.split())
+    WV.append((w, v))
+
+
+####################################
+
+n,w=map(int,input().split()
+WV=[]
+for i in range(n):
+    w,v=map(int,input().split()
+    WV+=[(w,v)]
+
+####################################
