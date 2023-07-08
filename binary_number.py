e289d7965dc7ddfb181e5e70b8f84333f6bc9a7e
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-07-08 10:45:20 +0900

    Update binary_number.py

--- binary_number.py
+++ binary_number.py
@@ -2,3 +2,9 @@ n = 4
 for i in range(2**n):
     binary = bin(i)[2:].zfill(n)
     print(binary)
+
+############################
+
+for i in range(9):
+    binary = bin(i)[2:].zfill(4)
+    print(binary)
