25fc8f922e5db4bdbb5b7d42bddb6e3cefcd9427
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-07-09 14:21:40 +0900

    Update input.py

--- input.py
+++ input.py
@@ -9,10 +9,16 @@ N,K=map(int,input().split())
 
 #########################################################
 
+import sys
+input = sys.stdin.readline
 
 #########################################################
 
+input = stdin.buffer.readline
 
+n, w = map(int, input().split())
+wv = [tuple(map(int, input().split())) for _ in range(n)]
+  
 #########################################################
 
 
