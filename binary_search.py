32305695bfc335c7b6c7f3c32dd3290d97b8e1d5
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-07-13 16:52:30 +0900

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -1,3 +1,4 @@
+#####################################################
 
 def binary_search(arr, target):
     left = 0
@@ -12,3 +13,28 @@ def binary_search(arr, target):
             right = mid - 1
     return -1
   
+#####################################################
+
+def binary_search(l, r):
+    while l < r:
+        mid = (l + r) // 2
+        # midを使った条件判定
+        if condition(mid):
+            r = mid
+        else:
+            l = mid + 1
+    return l
+
+# 二分探索で解を求める条件判定関数
+def condition(x):
+    # xを使った条件判定のロジックを実装
+    # 条件を満たす場合はTrueを、満たさない場合はFalseを返す
+    # 例: xがKより大きいかどうかを判定する場合
+    return x > K
+
+# 使用例
+N, K = 100, 50  # 探索範囲や条件に応じて値を設定
+result = binary_search(0, N)
+print(result)
+
+#####################################################
