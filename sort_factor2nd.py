d05d123cd24d678adf62e6d7b29ce5877fbd364f
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-07-25 16:28:12 +0900

    Update sort_factor2nd.py

--- sort_factor2nd.py
+++ sort_factor2nd.py
@@ -1,3 +1,11 @@
+############################################
 
 lst = [(a0,b0),(a1,b1),...]
 sorted_lst = sorted(lst, key=lambda x: x[1])
+
+############################################
+
+sorted_list = sorted(my_list, key=lambda x: x[1])
+sorted_list_descending = sorted(my_list, key=lambda x: x[1], reverse=True)
+
+############################################
