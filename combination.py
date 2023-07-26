5fd60099061788c6e22a5ff2724b1c0477b7e8af
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-07-26 15:56:26 +0900

    Update combination.py

--- combination.py
+++ combination.py
@@ -9,3 +9,19 @@ def combination(n, r):
         return 0
     else:
         return factorial(n) // (factorial(r) * factorial(n-r))
+
+######################################################################
+
+def factorial(n):
+    result = 1
+    for i in range(1, n+1):
+        result *= i
+    return result
+
+def combination(n, r):
+    if r > n:
+        return 0
+    else:
+        mod = 10**9 + 7
+        return factorial(n) // (factorial(n-r) * factorial(r)) % mod
+
