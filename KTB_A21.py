6bb17a076798e49360ff53ed7581ca5a1d648896
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-08-01 16:52:16 +0900

    Update KTB_A21.py

--- KTB_A21.py
+++ KTB_A21.py
@@ -8,15 +8,15 @@ N 個のブロックが並べられており、左から順に1,2,⋯,N と番�
 '''
 ##########################################################
 #stpete WA
+import itertools
+from itertools import permutations
 n=int(input())
-
 P=[]
 A=[]
 for i in range(n):
   pi,ai=map(int,input().split())
   P+=[pi]
   A+=[ai]
-  
 def Point(orders):
   N=list(range(1,n+1))
   points=0
@@ -27,10 +27,6 @@ def Point(orders):
       if q in N:
         points+=A[i-1]
   return points
-
-import itertools
-from itertools import permutations
-
 N2=list(range(1,n+1))
 C0=list(permutations(N2))
 POINTS=[]
@@ -38,6 +34,41 @@ for ci in C0:
   points=Point(list(ci))
   #print(ci,points)
   POINTS+=[points]
-  
 print(max(POINTS))
+##########################################################
+#stpete WA
+import sys
+input = sys.stdin.readline
+n=int(input())
+P=[None]*n
+A=[None]*n
+for i in range(n):
+  P[i],A[i]=map(int,input().split())
+N=list(range(1,n+1))
+DP=[]
+for i in range(n):
+  DP+=[[0]*n]
+AC=DP.copy()
+for i in range(n):
+  for j in range(n):
+    if 0<i+j<n:
+      Nij=N[i:n-j]
+      print(i,j,Nij)
+      if (i+j) not in Nij and P[i+j-1] in Nij:
+        DP[i][j]+=A[i+j-1]
+#print(DP)
+for i in range(n):
+  for j in range(n):
+    if i==0 and 0<i+j<n:
+      AC[i][j]=AC[i][j-1]+DP[i][j]
+    elif j==0 and 0<i+j<n:
+      AC[i][j]=AC[i-1][j]+DP[i][j]
+    elif 0<i+j<n:
+      AC[i][j]=max(AC[i-1][j]+DP[i][j],AC[i][j-1]+DP[i][j])
+#print(AC)
+MAX=0
+for i in range(n):
+  if MAX<max(AC[i]):
+    MAX=max(AC[i])
+print(MAX)
 ##########################################################
