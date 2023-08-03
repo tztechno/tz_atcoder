2e740f41c51803a992fdc67e0dee9d5fa244b003
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-08-03 22:01:45 +0900

    Update KTB_A21.py

--- KTB_A21.py
+++ KTB_A21.py
@@ -1,13 +1,5 @@
-#########################################################
-'''
-N 個のブロックが並べられており、左から順に1,2,⋯,N と番号が付けられています。
-以下の 2 種類の操作を何回か行うことで、すべてのブロックを取り除きたいです。
-今ある中で 一番左のブロックを取り除く。今ある中で 一番右のブロックを取り除く。
-ブロックi(i=1,2,⋯,N) をブロックPiより先に取り除いた場合、Ai点が得られます。
-合計得点としてあり得る最大値を出力する
-'''
 ##########################################################
-#stpete WA
+#stpete WA, use itertools
 import itertools
 from itertools import permutations
 n=int(input())
@@ -52,8 +44,8 @@ AC=DP.copy()
 for i in range(n):
   for j in range(n):
     if 0<i+j<n:
-      Nij=N[i:n-j]
-      print(i,j,Nij)
+      Nij=N[i:n-j]   ####
+      #print(i,j,Nij)
       if (i+j) not in Nij and P[i+j-1] in Nij:
         DP[i][j]+=A[i+j-1]
 #print(DP)
@@ -71,4 +63,46 @@ for i in range(n):
   if MAX<max(AC[i]):
     MAX=max(AC[i])
 print(MAX)
+##########################################################
+#stpete WA, use pop
+import sys
+input = sys.stdin.readline
+n=int(input())
+P=[None]*(n)
+A=[None]*(n)
+for i in range(n):
+  P[i],A[i]=map(int,input().split())
+N=list(range(1,n+1))
+#print('N',N)
+DP=[]
+for i in range(n):
+  DP+=[[0]*(n)]
+AC=DP.copy()
+def pop_left(lst, i):
+    return [lst.pop(0) for _ in range(i)]
+def pop_right(lst, j):
+    return [lst.pop() for _ in range(j)]
+for i in range(n):
+  for j in range(n):
+    if 0<i+j<n:
+      Nij=N.copy()
+      pop_left(Nij,i)      ####
+      pop_right(Nij,j)     ####
+      #print('Nij',i,j,Nij)
+      if (i+j) not in Nij and P[i+j-1] in Nij:
+        DP[i][j]+=A[i+j-1]
+#print(DP)
+for i in range(n):
+  for j in range(n):
+    if i==0 and 0<i+j<n:
+      AC[i][j]=AC[i][j-1]+DP[i][j]
+    elif j==0 and 0<i+j<n:
+      AC[i][j]=AC[i-1][j]+DP[i][j]
+    elif 0<i+j<n:
+      AC[i][j]=max(AC[i-1][j]+DP[i][j],AC[i][j-1]+DP[i][j])
+#print(AC)
+ANS=[]
+for i in range(n):
+  ANS+=[AC[n-1-i][i]]
+print(max(ANS))
 ##########################################################
