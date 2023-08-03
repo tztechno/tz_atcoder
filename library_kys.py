9a5eac75de443ee842f5dc5b31797d4f4676a07e
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-08-03 18:52:21 +0900

    Create library_kys.py

---
+++ library_kys.py
@@ -0,0 +1,13 @@
+from sys import stdin, setrecursionlimit
+setrecursionlimit(1000000)
+input = stdin.readline
+def iinput(): return int(input())
+def sinput(): return input().rstrip()
+def i0input(): return int(input()) - 1
+def linput(): return list(input().split())
+def liinput(): return list(map(int, input().split()))
+def miinput(): return map(int, input().split())
+def li0input(): return list(map(lambda x: int(x) - 1, input().split()))
+def mi0input(): return map(lambda x: int(x) - 1, input().split())
+INF = 1000000000000000000
+MOD = 1000000007
