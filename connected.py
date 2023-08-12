fcf681a87c62d9065f3846b362774235438dc3c1
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-08-12 16:48:19 +0900

    Update and rename xxx.py to connected.py

---
+++ connected.py
@@ -0,0 +1,31 @@
+def is_connected_graph(vertices, edges):
+    graph = {}
+    for vertex in vertices:
+        graph[vertex] = []
+
+    for edge in edges:
+        u, v = edge
+        graph[u].append(v)
+        graph[v].append(u)
+
+    visited = set()
+
+    def dfs(node):
+        visited.add(node)
+        for neighbor in graph[node]:
+            if neighbor not in visited:
+                dfs(neighbor)
+
+    start_vertex = vertices[0]
+    dfs(start_vertex)
+
+    return len(visited) == len(vertices)
+
+# 例として、頂点と辺の情報を設定
+vertices = [1, 2, 3, 4, 5]
+edges = [(1, 2), (2, 3), (3, 4), (4, 5)]
+
+if is_connected_graph(vertices, edges):
+    print("The graph is connected.")
+else:
+    print("The graph is not connected.")
