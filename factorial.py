7d501cf14ea794fbeb294f815f97a315b7fe5b47
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-08-16 16:56:28 +0900

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,5 +1,16 @@
+#####################################
+
+import math
+n = 5
+result = math.factorial(n)
+print(f"{n} の階乗は {result} です。")
+
+#####################################
+
 def factorial(n):
     if n == 0:
         return 1
     else:
         return n * factorial(n-1)
+        
+#####################################
