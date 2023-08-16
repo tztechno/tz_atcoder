492dddebd88de88417b577a7f2cc5560edf07843
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-08-16 16:58:05 +0900

    Update combination.py

--- combination.py
+++ combination.py
@@ -1,7 +1,12 @@
 ######################################################################
 
-
-
+import math
+def combination(n, r):
+    if r > n:
+        return 0
+    else:
+        mod = 10**9 + 7
+        return  math.factorial(n) // (math.factorial(n-r) * math.factorial(r)) % mod
 
 ######################################################################
 
