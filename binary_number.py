23bb2f4be54cf2ad857cd4a3be595b1db2cf2e7b
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-08-31 12:11:05 +0900

    Update binary_number.py

--- binary_number.py
+++ binary_number.py
@@ -1,3 +1,6 @@
+
+############################
+
 n = 4 
 for i in range(2**n):
     binary = bin(i)[2:].zfill(n)
@@ -8,3 +11,13 @@ for i in range(2**n):
 for i in range(9):
     binary = bin(i)[2:].zfill(4)
     print(binary)
+
+############################
+
+A = (1, 2, 3)
+B = (4, 5, 6)
+
+result = tuple(a | b for a, b in zip(A, B))
+print(result)  # 出力: (5, 7, 9)
+
+############################
