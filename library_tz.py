de5f01c92b1206c40b5c4721ce1ebd79ffbf48ea
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-09-03 21:30:08 +0900

    Update library_tz.py

--- library_tz.py
+++ library_tz.py
@@ -1,1 +1,16 @@
 
+_INPUT = """\
+
+"""
+if sys.argv[-1] == 'DEBUG':
+    import io
+    sys.stdin = io.StringIO(_INPUT)
+
+
+from collections import defaultdict,deque,Counter
+from itertools import product,permutations,combinations,accumulate
+from bisect import bisect_left,bisect_right,bisect
+from math import gcd,sqrt,log,exp,inf,pi,nan
+from heapq import heappush,heappop,heapify,heappushpop,heapreplace,merge,nlargest,nsmallest
+import sys
+input = sys.stdin.readline
