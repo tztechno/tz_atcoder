5f7c72d371d70c9025ee0edca9f81fb7df5be600
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-09-03 21:57:17 +0900

    Update library_tz.py

--- library_tz.py
+++ library_tz.py
@@ -1,6 +1,7 @@
 
 import sys
 input = sys.stdin.readline
+
 from math import gcd,sqrt,log,exp,inf,pi,nan
 from collections import defaultdict,deque,Counter
 from itertools import product,permutations,combinations,accumulate
