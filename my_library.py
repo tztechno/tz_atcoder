ec62304b2e1ac4c3fd2ab178a3f3d2d3328aa6bb
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-09-06 22:28:43 +0900

    Update my_library.py

--- my_library.py
+++ my_library.py
@@ -10,5 +10,8 @@ from heapq import heappush,heappop,heapify,heappushpop,heapreplace,merge,nlarges
 import networkx as nx
 
 INF = float("INF")
+FourNb=[(-1,0),(1,0),(0,1),(0,-1)]
+cursol=dict(zip('UDRL',FourNb))
 
+alp=[chr(ord('a')+i)for i in range(26)]
 
