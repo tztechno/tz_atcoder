3df1f966173662e1b9663bcda95ab5978fb02133
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-09-07 20:15:44 +0900

    Update KTB_A23.py

--- KTB_A23.py
+++ KTB_A23.py
@@ -1,5 +1,31 @@
 ##############################################
 #stpete
+#AC
+import sys
+input = sys.stdin.readline
+INF = float("INF")
+N,M=map(int,input().split())
+C=[]
+for i in range(M):
+  C+=[int(input().replace(' ',''),2)]
+#print(C)#クーポンを2進法で示す
+DP=[INF]*(1<<N)#使ったクーポンの枚数を格納
+DP[0]=0
+#print('a,c,a|c')
+for a in range(1<<N):# 既に確保した商品のリスト、何のクーポンを使ったか知らんけど
+  for c in C:#クーポンを追加で1枚使うことを想定
+    try:
+      #print(a,c,a|c)
+      DP[a|c]=min(DP[a|c],DP[a]+1)
+    except:
+      pass
+#print(DP)
+if DP[-1]==INF:
+  print(-1)
+else:
+  print(DP[-1])
+##############################################
+#stpete
 #TLE1
 import sys
 input = sys.stdin.readline
