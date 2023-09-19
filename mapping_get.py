6b593979157967bb7109f16fe735d64766dfc30b
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-09-19 10:53:35 +0900

    Update mapping_get.py

--- mapping_get.py
+++ mapping_get.py
@@ -1,7 +1,16 @@
-d = {'key1': 'val1', 'key2': 'val2', 'key3': 'val3'}
+####################################################
 
+d = {'key1': 'val1', 'key2': 'val2', 'key3': 'val3'}
 print(d.get('key1'))
 # val1
-
 print(d.get('key4'))
 # None
+
+####################################################
+
+class_names = ['paper', 'rock', 'scissors']
+N = list(range(len(class_names)))
+normal_mapping = dict(zip(class_names, N)) 
+reverse_mapping = dict(zip(N, class_names))    
+
+####################################################
