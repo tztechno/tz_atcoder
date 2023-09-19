a0a5bc77d98567b90a2a9b884a67aced8382e2d4
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-09-19 12:20:02 +0900

    Update find_combinations.py

--- find_combinations.py
+++ find_combinations.py
@@ -1,12 +1,17 @@
-def find_integer_combinations(N):
-    combinations = []
-    for i in range(1, N):
-        if i > N // 2:
-            break
-        j = N - i
-        combinations.append((i, j))
-    return combinations
+def find_integer_combinations(N, current_sum=0, current_combination=None):
+    if current_combination is None:
+        current_combination = []
 
-N = 10  # 和をNにしたい場合
-combinations = find_integer_combinations(N)
-print(combinations)
+    if current_sum == N:
+        return [tuple(current_combination)]
+    if current_sum > N:
+        return []
+
+    combinations = []
+    for i in range(1, N + 1):
+        if not current_combination or i >= current_combination[-1]:
+            new_combination = current_combination.copy()
+            new_combination.append(i)
+            combinations.extend(find_integer_combinations(N, current_sum + i, new_combination))
+
+    return combinations
