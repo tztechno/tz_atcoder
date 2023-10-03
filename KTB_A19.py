0ed9921d5b6317eb9b42176a21fec25aa6ddf514
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-10-03 15:25:32 +0900

    Update KTB_A19.py

--- KTB_A19.py
+++ KTB_A19.py
@@ -1,4 +1,5 @@
 #######################################################
+
 #stpete AC 
 N,WL=map(int,input().split())
 W=[0]
@@ -18,6 +19,39 @@ for i in range(1,N+1):
     else:
       DP[i][j]=DP[i-1][j]
 print(DP[N][WL])
+
 #######################################################
+
+#stpete TLE13/25
+
+import numpy as np
+
+N,w = map(int,input().split())
+W=[]
+V=[]
+for i in range(N):
+    wi,vi = list(map(int,input().split()))
+    W+=[wi]
+    V+=[vi]
+
+DPWV=[]
+for i in range(1<<N):
+    DPWV+=[[]]
+
+normal_mapping={'0':0,'1':1}
+reverse_mapping={0:'0',1:'1'}
+
+for i in range(1<<N):
+    bi=list(bin(i)[2:].zfill(N))
+    B=[]
+    for bii in bi:
+        B+=[normal_mapping[bii]]
+    DPWV[i]=((np.array(W)*np.array(B)).sum(),(np.array(V)*np.array(B)).sum())
+
+maxv=0
+for dp in DPWV:
+    if dp[0]<=w:
+        maxv=max(maxv,dp[1])
+print(maxv)
 
 #######################################################
