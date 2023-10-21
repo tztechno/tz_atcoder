5c44f5b946ccf53906ab3f66600523432674fab6
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-10-22 00:59:13 +0900

    Update and rename sort_factor2nd.py to sort_factor.py

--- sort_factor.py
+++ sort_factor.py
@@ -9,3 +9,19 @@ sorted_list = sorted(my_list, key=lambda x: x[1])
 sorted_list_descending = sorted(my_list, key=lambda x: x[1], reverse=True)
 
 ############################################
+
+from operator import itemgetter
+from heapq import heappop,heappush
+
+N=int(input())
+TD=[list(map(int,input().split())) for i in range(N)]
+
+for i in range(N):
+    TD[i][1]+=TD[i][0]
+
+print(TD)
+
+TD.sort(key=itemgetter(0))
+
+############################################
+
