3c028da52907b04995bc14919f1a13f4b876acee
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-10-25 00:34:54 +0900

    Update defaultdict.py

--- defaultdict.py
+++ defaultdict.py
@@ -18,3 +18,24 @@ my_dict['cherry'] += 1
 
 # デフォルト値は0から1に更新されます
 print(my_dict['cherry'])  # 出力: 1
+
+######################################
+
+from collections import defaultdict
+
+# defaultdictの初期化
+# デフォルト値として空のリストを指定しています
+dic = defaultdict(lambda: [])
+
+# 新しいキーにアクセス
+# 存在しないキーを参照しても、空のリストが自動的に割り当てられます
+dic['fruits'].append('apple')
+
+# 存在しないキー 'fruits' が自動的に作成され、リストに 'apple' が追加されます
+print(dic)  # 出力: defaultdict(<function <lambda> at 0x7f386a8cc950>, {'fruits': ['apple']})
+
+# 別のキーにアクセス
+# 存在しないキー 'vegetables' が自動的に作成され、空のリストに 'carrot' が追加されます
+dic['vegetables'].append('carrot')
+print(dic)  # 出力: defaultdict(<function <lambda> at 0x7f386a8cc950>, {'fruits': ['apple'], 'vegetables': ['carrot']})
+
