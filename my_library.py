3daf58f21db336bd7e5d34f52dbebc37a94d3ef2
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-11-10 12:43:16 +0900

    Update my_library.py

--- my_library.py
+++ my_library.py
@@ -9,6 +9,8 @@ from itertools import product,permutations,combinations,accumulate
 from bisect import bisect_left,bisect_right,bisect
 from heapq import heappush,heappop,heapify,heappushpop,heapreplace,merge,nlargest,nsmallest
 import networkx as nx
+from collections import defaultdict
+cnt = defaultdict(int)
 
 INF = float("INF")
 FourNb=[(-1,0),(1,0),(0,1),(0,-1)]
