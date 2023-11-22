0540ef2e15781e83dc68da0ae649d84931244442
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-11-22 11:33:05 +0900

    Update deque.py

--- deque.py
+++ deque.py
@@ -2,6 +2,32 @@
 
 from collections import deque
 
+左から取る (popleft()):
+my_deque = deque([1, 2, 3, 4, 5])
+popped_element = my_deque.popleft()
+print(popped_element)  # 最初の要素が取り出され、出力される
+print(my_deque)  # deque([2, 3, 4, 5])
+
+右から取る (pop()):
+my_deque = deque([1, 2, 3, 4, 5])
+popped_element = my_deque.pop()
+print(popped_element)  # 最後の要素が取り出され、出力される
+print(my_deque)  # deque([1, 2, 3, 4])
+
+左に足す (appendleft()):
+my_deque = deque([2, 3, 4, 5])
+my_deque.appendleft(1)  # 最初に要素を追加する
+print(my_deque)  # deque([1, 2, 3, 4, 5])
+
+右に足す (append()):
+my_deque = deque([1, 2, 3, 4])
+my_deque.append(5)  # 末尾に要素を追加する
+print(my_deque)  # deque([1, 2, 3, 4, 5])
+
+##############################################
+
+from collections import deque
+
 # 空の deque の作成
 my_deque = deque()
 
