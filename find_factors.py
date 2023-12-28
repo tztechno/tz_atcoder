22e0f2c1333ae5057ef5673d3b7592a3ab5933a9
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-28 17:25:20 +0900

    Update find_factors.py

--- find_factors.py
+++ find_factors.py
@@ -8,3 +8,23 @@ def find_factors(n):
             b = n // a
             factors.append((a, b))
     return factors
+
+
+def prime_factorization(n):
+    factors = []
+    # 2で割り切れる限り割り続ける
+    while n % 2 == 0:
+        factors.append(2)
+        n = n // 2
+
+    # 3以上の奇数で割り切れる限り割り続ける
+    for i in range(3, int(n**0.5) + 1, 2):
+        while n % i == 0:
+            factors.append(i)
+            n = n // i
+
+    # nが素数の場合
+    if n > 2:
+        factors.append(n)
+
+    return factors
