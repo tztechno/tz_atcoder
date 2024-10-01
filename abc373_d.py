dc7ed52fbc3c6b4323a657e7f2e255cf41b87632
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-10-01 22:40:42 +0900

    Update abc373_d.py

--- abc373_d.py
+++ abc373_d.py
@@ -6,8 +6,73 @@ abc373_d.py
 ######################################################
 ######################################################
 ######################################################
+
+# 入力を読み込む
+N, M = map(int, input().split())
+G = [[] for _ in range(N)]
+for _ in range(M):
+    u, v, w = map(int, input().split())
+    u -= 1
+    v -= 1
+    G[u].append((v, w))
+    G[v].append((u, -w))
+
+visited = [False] * N  # 値が確定しているかどうか
+ans = [0] * N  # 書き込む値
+for i in range(N):
+    # すでに値が確定しているならばスキップ
+    if visited[i]:
+        continue
+    # 頂点 i の値を 0 に確定し，探索を始める
+    st = [i]
+    visited[i] = True
+    while st:
+        # いま頂点 u にいる
+        u = st.pop()
+        # 頂点 u に隣接する頂点 v を調べる
+        for v, w in G[u]:
+            if not visited[v]:
+                # まだ頂点 v の値が確定していないならば，頂点 u の値と整合的になるように，頂点 v の値を確定させる
+                visited[v] = True
+                ans[v] = ans[u] + w
+                st.append(v)
+print(*ans)
+
 ######################################################
 
+from heapq import heapify, heappop, heappush
+
+n,m = map(int, input().split())
+adjacents = {i: [[], []] for i in range(1, n+1)}
+
+
+for _ in range(m):
+    u,v, w = map(int, input().split())
+    adjacents[u][0].append((v, w))
+    adjacents[v][1].append((u, w))
+
+values = {}
+
+for i in range(1, n+1):
+    if len(values.keys()) >=n: break
+    if i in values: continue
+    heap = [i]
+    values[i] = 0
+    while len(heap) >0:
+        last = heappop(heap)
+        for e, w in adjacents[last][1]:
+            if e in values: continue
+            values[e] = values[last] - w
+            heappush(heap, e)
+
+        for e, w in adjacents[last][0]:
+            if e in values: continue
+            values[e] = values[last] + w
+            heappush(heap, e)
+
+for i in range(1, n+1):
+    print(values[i], end=" ")
+  
 ######################################################
 [PARTIALY UNDERSTAND]
 
