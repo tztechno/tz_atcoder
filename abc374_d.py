5d5412b7b7772ac6624c96d58b2d670dfdc58346
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-10-05 22:46:05 +0900

    Update abc374_d.py

--- abc374_d.py
+++ abc374_d.py
@@ -6,6 +6,44 @@ abc374_d.py
 ########################################################################################
 ########################################################################################
 ########################################################################################
+
+import sys
+input = sys.stdin.readline
+from itertools import permutations
+
+N,S,T=map(int,input().split())
+A=[list(map(int,input().split())) for i in range(N)]
+L=list(permutations(range(N)))
+ANS=1<<63
+
+for LIST in L:
+    score=0
+    for j in range(2**N):
+        score=0
+        for i in range(len(LIST)):    
+            if i==0:
+                z,w=0,0
+            else:
+                if j & (1<<(i-1)) != 0:
+                    z,w=A[LIST[i-1]][2:]
+                else:
+                    z,w=A[LIST[i-1]][:2]
+
+            if j & (1<<i) != 0:
+                a,b,c,d=A[LIST[i]]
+            else:
+                c,d,a,b=A[LIST[i]]
+
+            score+=(((c-a)**2+(d-b)**2)**(1/2))/T
+            score+=(((z-a)**2+(w-b)**2)**(1/2))/S
+
+        #print(score)
+
+        if ANS>score:
+            ANS=score
+
+print(ANS)
+
 ########################################################################################
 [my WA]
 N,S,T=map(int,input().split())
