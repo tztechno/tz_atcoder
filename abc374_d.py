51a61ec0facaaf774940f9db9f13e2f58161d4a2
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-10-07 14:59:29 +0900

    Update abc374_d.py

--- abc374_d.py
+++ abc374_d.py
@@ -4,7 +4,92 @@ abc374_d.py
 ########################################################################################
 ########################################################################################
 ########################################################################################
+
+import sys
+
+sys.setrecursionlimit(int(1e6))
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+
+
+from functools import lru_cache
+from math import sqrt
+
+
+INF = int(4e18)
+
+
+def min2(a, b):
+    return a if a < b else b
+
+
+if __name__ == "__main__":
+    N, S, T = map(int, input().split())
+    A, B, C, D = [], [], [], []
+    for _ in range(N):
+        a, b, c, d = map(int, input().split())
+        A.append(a)
+        B.append(b)
+        C.append(c)
+        D.append(d)
+
+    def calc(x1, y1, x2, y2):
+        return sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+
+    T1 = [calc(A[i], B[i], C[i], D[i]) / T for i in range(N)]
+
+    MASK = (1 << N) - 1
+
+    @lru_cache(None)
+    def dfs(visited: int, x: int, y: int) -> float:
+        if visited == MASK:
+            return 0
+        res = INF
+        for next_ in range(N):
+            if (visited >> next_) & 1:
+                continue
+            a, b, c, d = A[next_], B[next_], C[next_], D[next_]
+            res = min2(res, calc(x, y, a, b) / S + T1[next_] + dfs(visited | (1 << next_), c, d))
+            res = min2(res, calc(x, y, c, d) / S + T1[next_] + dfs(visited | (1 << next_), a, b))
+        return res
+
+    res = dfs(0, 0, 0)
+    dfs.cache_clear()
+    print(res)
+
 ########################################################################################
+
+from itertools import permutations
+import math
+inf=float("inf")
+N,S,T=map(int,input().split())
+L=[]
+for i in range(N):
+    a,b,c,d=map(int,input().split())
+    L.append([a,b,c,d])
+#print(L)
+
+def dis(x0,y0,x1,y1):
+    res=math.sqrt((x0-x1)**2+(y0-y1)**2)
+    return res
+
+ans=inf
+for p in list(permutations(range(N))):
+#    print(p)
+    for bit in range(1<<N):
+        x,y=0,0
+        s=0
+        for i in range(N):
+            a,b,c,d=L[p[i]]
+            if bit & (1<<i):
+                s+=dis(x,y,c,d)/S
+                s+=dis(a,b,c,d)/T
+                x,y=a,b
+            else:
+                s+=dis(x,y,a,b)/S
+                s+=dis(a,b,c,d)/T
+                x,y=c,d
+        ans=min(ans,s)
+print(ans)
 ########################################################################################
 [not understand]
 
