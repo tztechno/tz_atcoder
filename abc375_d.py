d9315097168514a156cb7b1e062832b6a72bcdad
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-10-13 22:06:36 +0900

    Update abc375_d.py

--- abc375_d.py
+++ abc375_d.py
@@ -5,7 +5,102 @@
 #################################################################
 #################################################################
 #################################################################
+
+from collections import defaultdict
+
+
+def do():
+    all_count = 0
+    S = input()
+
+    right_dict = defaultdict(int)
+    for i in range(len(S)):
+        s_i = S[i]
+        right_dict[s_i] += 1
+
+    left_dict = defaultdict(int)
+    for j in range(len(S)):
+        s_j = S[j]
+        right_dict[s_j] -= 1
+        for s_i, count in left_dict.items():
+            all_count += (count * right_dict[s_i])
+        left_dict[s_j] += 1
+
+    print(all_count)
+    return
+
+
+if __name__ == '__main__':
+    do()
+
 #################################################################
+
+import string
+from collections import defaultdict
+from collections import Counter
+
+S = list(input())
+lcnt = defaultdict(int)
+rcnt = defaultdict(int)
+rcnt = Counter(S[1:])
+ans = 0
+for i in range(1, len(S) - 1):
+    lcnt[S[i - 1]] += 1
+    rcnt[S[i]] -= 1
+    #print(S[i])
+    #print(lcnt)
+    #print(rcnt)
+    for j in list(string.ascii_uppercase):
+        ans += lcnt[j] * rcnt[j]
+print(ans)
 #################################################################
+
+S = list(input())
+
+l = [0 for _ in range(26)]
+r = [0 for _ in range(26)]
+for s in S:
+    r[ord(s) - ord("A")] += 1
+
+oldc = ord(S[0]) - ord("A")
+l[oldc] -= 1
+ans = 0
+for s in S:
+    c = ord(s) - ord("A")
+    l[oldc] += 1
+    r[c] -= 1
+    
+    for i in range(26):
+        ans += l[i] * r[i]
+    oldc = c
+
+print(ans)
+    
 #################################################################
+
+import sys
+input = sys.stdin.readline
+
+S=input().strip()
+A=[]
+for s in S:
+    A.append(ord(s)-65)
+ANS=0
+
+DP=[0]*26
+DP2=[0]*26
+
+for i in range(len(A)):
+    #print(ANS,DP,DP2)
+
+    ANS+=DP2[A[i]]
+    for j in range(26):
+        DP2[j]+=DP[j]
+    DP[A[i]]+=1
+    
+
+print(ANS)
+    
+
+
 #################################################################
