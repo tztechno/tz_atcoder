1e0f2d2a664baed92e9a0dd3fc7b02d750bd24e1
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-10-26 23:56:52 +0900

    Update abc377_d.py

--- abc377_d.py
+++ abc377_d.py
@@ -2,4 +2,30 @@
 #####################################################
 #####################################################
 #####################################################
+
+import sys
+input = sys.stdin.readline
+
+N,M=map(int,input().split())
+
+LR=[list(map(int,input().split())) for i in range(N)]
+
+LIST=[[] for i in range(M+2)]
+
+for l,r in LR:
+    LIST[l].append(r)
+
+ANS=0
+MIN=M+1
+
+for i in range(M,0,-1):
+    for r in LIST[i]:
+        MIN=min(MIN,r)
+
+    ANS+=MIN-i
+
+    #print(i,MIN,ANS)
+
+print(ANS)
+
 #####################################################
