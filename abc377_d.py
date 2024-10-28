07432d5b8caa3ec6975b57f3528f5a99c4993005
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-10-28 14:41:05 +0900

    Update abc377_d.py

--- abc377_d.py
+++ abc377_d.py
@@ -1,6 +1,25 @@
 #####################################################
 #####################################################
 #####################################################
+[my WA 2024-10-28]
+
+import sys
+input = sys.stdin.readline
+N,M=map(int,input().split())
+A=set(range(1,M+1))
+for i in range(N):
+  l,r=map(int,input().split())
+  if l==r:
+    M-=1
+  elif l<r:
+    for j in range(l,r):
+      A-=set([j])
+if M==0:
+  print(0)
+else:
+  print(len(A)+M)
+
+
 #####################################################
 
 import sys
