7a6c74ef847581e5c14d4dacbf35b5b16c87d106
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-11-09 20:06:59 +0900

    Update abc264_c.py

--- abc264_c.py
+++ abc264_c.py
@@ -71,9 +71,12 @@ print("No")
 
 ##########################################################
 
-[CGPT 相談　WA,TLE15]
 
-from itertools import product
+##########################################################
+
+[MY ANS TLE4]
+
+from itertools import combinations
 
 H1, W1 = map(int, input().split()) 
 A0 = [list(map(int, input().split())) for _ in range(H1)] 
@@ -81,80 +84,17 @@ A0 = [list(map(int, input().split())) for _ in range(H1)]
 H2, W2 = map(int, input().split()) 
 B0 = [list(map(int, input().split())) for _ in range(H2)]  
 
-for a_indices in product(range(H1), repeat=H2): 
-    for b_indices in product(range(W1), repeat=W2): 
-        match = True
-        for i in range(H2):
-            for j in range(W2):
-                if A0[a_indices[i]][b_indices[j]] != B0[i][j]:
-                    match = False
-                    break
-            if not match:
-                break
-        if match:
-            print("Yes")
-            exit()
-print("No")
-
-
-##########################################################
-
-[MY ANS TLE4]
-
 import numpy as np
-from itertools import product,permutations,combinations,accumulate
-
-H1,W1=map(int,input().split())
-A0=[]
-for i in range(H1):
-  A0+=[list(map(int,input().split()))]
-H2,W2=map(int,input().split())
-B0=[]
-for i in range(H2):
-  B0+=[list(map(int,input().split()))]
-
 A=np.array(A0)
 B=np.array(B0)
 
-H=set()
-for i in range(H1):
-  Ai=A[i]
-  for j in range(H2):
-    Bj=B[j]
-    if all(element in Ai for element in Bj):
-      H.add(i)
-if H==set():
-  print('No')
-  exit()
-
-A2=A[list(H)]
-
-W=set()
-for i in range(W1):
-  Ai=A2[:,i]
-  for j in range(W2):
-    Bj=B[:,j]
-    if all(element in Ai for element in Bj):
-      W.add(i)
-if W==set():
-  print('No')
-  exit()
-  
-A3=A2[:,list(W)]
-#print(A3.shape)
-
-CH=list(combinations(list(range(A3.shape[0])),H2))
-CW=list(combinations(list(range(A3.shape[1])),W2))
-CP=list(product(CH,CW))
-#print(A2)
-for c in CP:
-  #print(c)
-  Ai=A3[list(c[0])]
-  Ai2=Ai[:,list(c[1])]
-
-  if np.array_equal(Ai2,B):
-    print('Yes')
-    exit()
+for a_indices in combinations(range(H1), H2): 
+  for b_indices in combinations(range(W1), W2): 
+    A2=A[list(a_indices)]
+    A3=A2[:,list(b_indices)]
+    if np.all(np.equal(A3,B)):
+      print('Yes')
+      exit()
 else:
   print('No')
   
