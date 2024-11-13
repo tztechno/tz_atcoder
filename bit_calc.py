4e71b23047bcd79d760a49f787ec450b15259824
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-11-13 22:45:44 +0900

    Update bit_calc.py

--- bit_calc.py
+++ bit_calc.py
@@ -14,3 +14,23 @@ if not mask & (1<<i):
 # iがmaskに含まれないならば
   
 ####################################
+
+a_candidates = [i for i in range(1 << H1) if bin(i).count('1') == H2]
+b_candidates = [i for i in range(1 << W1) if bin(i).count('1') == W2]
+for a_mask in a_candidates:
+    for b_mask in b_candidates:
+        a_ind = [i for i in range(H1) if (a_mask >> i) & 1]
+        b_ind = [j for j in range(W1) if (b_mask >> j) & 1]
+      
+        A2 = [A0[i] for i in a_ind]                    
+        A3 = [[row[j] for j in b_ind] for row in A2]   
+
+--------------------------------------
+
+for a_ind in combinations(range(H1), H2):
+    for b_ind in combinations(range(W1), W2):
+      
+        A2 = [A0[i] for i in a_ind]                   
+        A3 = [[row[j] for j in b_ind] for row in A2]  
+  
+####################################
