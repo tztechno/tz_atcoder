a107074d44ce431475282890ceb78cf4a9f7c85b
stpeteishii <47345279+tztechno@users.noreply.github.com>
2025-02-06 17:54:11 +0900

    Create abc387_c.py

---
+++ abc387_c.py
@@ -0,0 +1,32 @@
+#################################################
+
+#################################################
+
+#################################################
+
+#################################################
+
+#################################################
+[my TLE]
+
+import sys
+input = sys.stdin.readline
+l,r=map(int,input().split())
+
+from collections import defaultdict,deque,Counter
+
+def judge(x):
+  X=list(str(x))
+  C=Counter(X)
+  mx=max(C.keys())
+  if C[mx]==1 and X[0]==str(mx):
+    return 1
+  else:
+    return 0
+
+ans=0
+for i in range(l,r+1):
+  ans+=judge(i)
+print(ans)
+#################################################
+
