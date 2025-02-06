3b5f6763557929044049cbfe8639b8bef1a19ddc
stpeteishii <47345279+tztechno@users.noreply.github.com>
2025-02-06 19:16:36 +0900

    Update abc387_c.py

--- abc387_c.py
+++ abc387_c.py
@@ -2,6 +2,73 @@
 #################################################
 #################################################
 #################################################
+[titia]
+
+import sys
+input = sys.stdin.readline
+
+#L,R=map(int,input().split())
+
+def calc(x):
+    ANS=0
+    for keta  in range(1,19):
+        for head in range(1,10):
+            #print(head*(10**keta),ANS)
+            if x//(10**keta)<head:
+                break
+
+            elif x//(10**keta)>head:
+                #print("!?",x,(head+1)*(10**keta))
+                ANS+=head**keta
+            else:
+                #print("!!",keta,head,ANS)
+                flag=1
+                S=str(x)
+                for j in range(1,len(S)):
+                    p=int(S[j])
+                    #print(p,head,keta,j,min(head,p)**(keta+1-j),ANS)
+
+                    ANS+=min(head,p)*(head**(keta-j))
+
+                    if p>=head:
+                        flag=0
+                        break
+
+                    #print(ANS)
+
+                if hebi(x):
+                    ANS+=1
+
+    return ANS
+
+def hebi(x):
+    if x==0:
+        return 0
+    S=str(x)
+    head=int(S[0])
+
+    for i in range(1,len(S)):
+        if head<=int(S[i]):
+            return False
+
+    return True
+
+def calc2(x):
+    ANS=0
+    for i in range(10,x+1):
+        if hebi(i)==True:
+            ANS+=1
+    return ANS
+
+L,R=map(int,input().split())
+
+print(calc(R)-calc(L-1))
+
+for i in range(1,1010):
+    #print(i)
+    assert calc(i)==calc2(i)
+        
+    
 
 #################################################
 #################################################
