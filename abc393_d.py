03869e491de618037cea163fd7dceee2ea4a2f3c
stpeteishii <47345279+tztechno@users.noreply.github.com>
2025-03-08 19:23:51 +0900

    Update abc393_d.py

--- abc393_d.py
+++ abc393_d.py
@@ -2,6 +2,50 @@
 ##################################################################
 ##################################################################
 ##################################################################
+[cgpt]
+
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n + 1))  # 初期状態では自分自身が親
+        self.rank = [0] * (n + 1)
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])  # 経路圧縮
+        return self.parent[x]
+
+    def union(self, x, y):
+        root_x = self.find(x)
+        root_y = self.find(y)
+        if root_x != root_y:
+            if self.rank[root_x] < self.rank[root_y]:
+                self.parent[root_x] = root_y
+            elif self.rank[root_x] > self.rank[root_y]:
+                self.parent[root_y] = root_x
+            else:
+                self.parent[root_y] = root_x
+                self.rank[root_x] += 1
+
+N, Q = map(int, input().split())
+uf = UnionFind(N)
+
+for _ in range(Q):
+    qr = list(map(int, input().split()))
+
+    if qr[0] == 1:
+        a, b = qr[1], qr[2]
+        uf.parent[a] = b  # 鳩 a の巣を直接 b にする
+
+    elif qr[0] == 2:
+        a, b = qr[1], qr[2]
+        uf.union(a, b)  # 巣 a と巣 b を統合する
+
+    elif qr[0] == 3:
+        a = qr[1]
+        print(uf.find(a))  # 鳩 a のいる巣を出力
+
+
+
 ##################################################################
 [titia]
 
