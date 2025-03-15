0aee951de47f72e7f461f364fe0d19be6a302997
stpeteishii <47345279+tztechno@users.noreply.github.com>
2025-03-15 17:12:04 +0900

    Update abc396_c.py

--- abc396_c.py
+++ abc396_c.py
@@ -5,6 +5,40 @@
 ##################################################
 
 ##################################################
+[titia]
+
+import sys
+input = sys.stdin.readline
+
+N,M=map(int,input().split())
+B=list(map(int,input().split()))
+W=list(map(int,input().split()))
+
+
+B.sort(reverse=True)
+W.sort(reverse=True)
+
+SB=[0]
+for b in B:
+    SB.append(SB[-1]+b)
+
+SW=[0]
+for w in W:
+    SW.append(SW[-1]+w)
+
+
+for i in range(1,len(SW)):
+    SW[i]=max(SW[i-1],SW[i])
+
+while len(SW)<len(SB):
+    SW.append(SW[-1])
+
+ANS=0
+
+for i in range(len(SB)):
+    ANS=max(ANS,SB[i]+SW[i])
+
+print(ANS)
 
 ##################################################
 [cgpt AC]
