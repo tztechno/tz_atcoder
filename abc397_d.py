bd77bc6032340ff3affad178d805a6f9d5bc2577
stpeteishii <47345279+tztechno@users.noreply.github.com>
2025-03-16 13:57:32 +0900

    Update abc397_d.py

--- abc397_d.py
+++ abc397_d.py
@@ -4,7 +4,48 @@
 ###################################################
 ###################################################
 ###################################################
+[tetsu]
+
+def f(a, b):
+    return a*a*a-b*b*b
+
+N=int(input())
+
+for w in range(1,1000000):
+    ok=1
+    ng=int(1e9)
+    while (ng-ok)>1:
+        m=(ok+ng)//2
+        if(f(w+m,m)<=N): ok=m
+        else: ng=m
+
+    if(f(ok+w,ok)==N):
+        print(ok+w,ok)
+        exit()
+print(-1)
+
 ###################################################
+[titia]
+
+import sys
+input = sys.stdin.readline
+from math import sqrt
+
+N=int(input())
+
+for k in range(1,10**6+10):
+    a=3*k
+    b=3*k*k
+    c=k*k*k-N
+    if b*b-4*a*c>=0:
+        y=(-b+sqrt(b*b-4*a*c))/(2*a)
+        y=round(y)
+        x=y+k
+        if x>0 and y>0 and x*x*x-y*y*y==N:
+            print(x,y)
+            exit()
+print(-1)
+
 ###################################################
 [my TLE,RE31]
 N=int(input())
