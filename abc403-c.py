3f81ff1a596cce875c4f0c5a963bb87080a8dc01
stpeteishii <47345279+tztechno@users.noreply.github.com>
2025-04-28 18:55:27 +0900

    Update abc403-c.py

--- abc403-c.py
+++ abc403-c.py
@@ -11,14 +11,126 @@
 ##################################################################
 
 ##################################################################
+[my AC]
+
+N,M,Q=map(int,input().split())
+A=set()
+ALL=set()
+for i in range(Q):
+  qr=list(map(int,input().split()))
+  if qr[0]==1:
+    x,y=qr[1],qr[2]
+    A.add((x-1,y-1))
+  elif qr[0]==2:
+    x=qr[1]
+    ALL.add(x-1)
+  else:
+    x,y=qr[1],qr[2]
+    if x-1 in ALL:
+      print('Yes')
+    elif (x-1,y-1) in A:
+      print('Yes')
+    else:
+      print('No')
+
 
 ##################################################################
+[my TLE3]
+
+N,M,Q=map(int,input().split())
+A=set()
+ALL=[]
+for i in range(Q):
+  qr=list(map(int,input().split()))
+  if qr[0]==1:
+    x,y=qr[1],qr[2]
+    A.add((x-1,y-1))
+  elif qr[0]==2:
+    x=qr[1]
+    ALL+=[x-1]
+  else:
+    x,y=qr[1],qr[2]
+    if x-1 in ALL:
+      print('Yes')
+    elif (x-1,y-1) in A:
+      print('Yes')
+    else:
+      print('No')
 
 ##################################################################
+[my TLE20]
+
+N,M,Q=map(int,input().split())
+A=[]
+
+ALL=[]
+for i in range(Q):
+  qr=list(map(int,input().split()))
+  if qr[0]==1:
+    x,y=qr[1],qr[2]
+    A+=[(x-1,y-1)]
+  elif qr[0]==2:
+    x=qr[1]
+    ALL+=[x-1]
+  else:
+    x,y=qr[1],qr[2]
+    if x-1 in ALL:
+      print('Yes')
+    elif (x-1,y-1) in A:
+      print('Yes')
+    else:
+      print('No')
 
 ##################################################################
+[my AC]
+
+N,M,Q=map(int,input().split())
+A=[]
+for i in range(N):
+  A+=[set()]
+ALL=set()
+for i in range(Q):
+  qr=list(map(int,input().split()))
+  if qr[0]==1:
+    x,y=qr[1],qr[2]
+    A[x-1].add(y-1)
+  elif qr[0]==2:
+    x=qr[1]
+    ALL.add(x-1)
+  else:
+    x,y=qr[1],qr[2]
+    if x-1 in ALL:
+      print('Yes')
+    elif y-1 in A[x-1]:
+      print('Yes')
+    else:
+      print('No')
+
 
 ##################################################################
+[my TLE3]
+
+N,M,Q=map(int,input().split())
+A=[]
+for i in range(N):
+  A+=[set()]
+ALL=[]
+for i in range(Q):
+  qr=list(map(int,input().split()))
+  if qr[0]==1:
+    x,y=qr[1],qr[2]
+    A[x-1].add(y-1)
+  elif qr[0]==2:
+    x=qr[1]
+    ALL+=[x-1]
+  else:
+    x,y=qr[1],qr[2]
+    if x-1 in ALL:
+      print('Yes')
+    elif y-1 in A[x-1]:
+      print('Yes')
+    else:
+      print('No')
 
 ##################################################################
 [titia]
@@ -27,7 +139,6 @@ import sys
 input = sys.stdin.readline
 
 N,M,Q=map(int,input().split())
-
 OK=[0]*(N+1)
 S=set()
 
