6c7e90bd676dcaa3d8ceae25e6f349498246cf45
stpeteishii <47345279+tztechno@users.noreply.github.com>
2025-09-18 13:06:12 +0900

    Update abc385_c.py

--- abc385_c.py
+++ abc385_c.py
@@ -106,6 +106,29 @@ print(ans)
 ###############################################
 ###############################################
 ###############################################
+[titia]
+import sys
+input = sys.stdin.readline
+
+n=int(input())
+A=list(map(int,input().split()))
+
+ANS=1
+for i in range(n):
+    for j in range(1,n):
+        now=i
+        score=0
+        while True:
+            if now<n and A[now]==A[i]:
+                score+=1
+            else:
+                break
+            now+=j
+
+        ANS=max(ANS,score)
+
+print(ANS)
+
 ###############################################
 ###############################################
 ###############################################
