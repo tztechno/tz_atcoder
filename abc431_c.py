d30556e802862dfbb2c1f8b367d368acdf5f912b
stpeteishii <47345279+tztechno@users.noreply.github.com>
2025-11-09 23:56:45 +0900

    Update abc431_c.py

--- abc431_c.py
+++ abc431_c.py
@@ -1,6 +1,31 @@
 ##################################################################
 ##################################################################
 ##################################################################
+[mk]
+N, M, K = [int(x) for x in input().split()]
+H = sorted([int(x) for x in input().split()])
+B = sorted([int(x) for x in input().split()])
+i, j = 0, 0
+ans = 0
+while i < N and j < M:
+    if H[i] <= B[j]: 
+        ans += 1
+        i += 1
+        j += 1
+    else:
+        j += 1
+print('Yes' if ans >= K else 'No')
+##################################################################
+[dho]
+N,M,K=map(int,input().split())
+H=list(map(int,input().split()))
+B=list(map(int,input().split()))
+if min(N,M)<K: print('No'); exit()
+H,B=sorted(H),sorted(B)
+H,B=H[:K],B[M-K:]
+for i in range(K):
+    if H[i]>B[i]: print('No'); exit()
+print('Yes')
 ##################################################################
 ##################################################################
 ##################################################################
@@ -10,8 +35,49 @@
 ##################################################################
 ##################################################################
 ##################################################################
+[TLE27]
+n,m,k=map(int,input().split())
+H=list(map(int,input().split()))#n
+W=list(map(int,input().split()))#m
+from collections import defaultdict,deque,Counter
+from bisect import insort, bisect_left, bisect_right
+H.sort()
+CW=Counter(W)
+cwkey=sorted(CW.keys())
+t=0
+for cw in cwkey:
+  H2=H.copy()
+  idx=bisect_right(H2,cw)
+  mn=min(idx-t,CW[cw])
+  t+=mn
+  CW[cw]-=mn
+if t>=k:
+  print('Yes')
+else:
+  print('No')
 ##################################################################
-##################################################################
+[TLE28]
+n,m,k=map(int,input().split())
+H=list(map(int,input().split()))#n
+W=list(map(int,input().split()))#m
+from collections import defaultdict,deque,Counter
+from bisect import insort, bisect_left, bisect_right
+H.sort()
+CW=Counter(W)
+cwkey=sorted(CW.keys())
+t=0
+for cw in cwkey:
+  H2=H.copy()
+  idx=bisect_right(H2,cw)
+  mn=min(idx,CW[cw])
+  t+=mn
+  CW[cw]-=mn
+  H=H2[mn:]
+if t>=k:
+  print('Yes')
+else:
+  print('No')
+
 ##################################################################
 [TLE29]
 n,m,k=map(int,input().split())
