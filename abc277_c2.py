a5c1a154ace9e67cb0fa9aabe6aaae9e161f8522
stpeteishii <47345279+tztechno@users.noreply.github.com>
2025-11-11 19:05:38 +0900

    Update abc277_c2.py

--- abc277_c2.py
+++ abc277_c2.py
@@ -39,6 +39,33 @@ print(max(list(M)))
 ###############################################
 ###############################################
 ###############################################
+[deepseek]
+from collections import defaultdict, deque
+
+def main():
+    n = int(input())
+    graph = defaultdict(list)
+    
+    for _ in range(n):
+        a, b = map(int, input().split())
+        graph[a].append(b)
+        graph[b].append(a)
+    
+    visited = set([1])
+    queue = deque([1])
+    
+    while queue:
+        current = queue.popleft()
+        
+        for neighbor in graph[current]:
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.append(neighbor)
+    
+    print(max(visited))
+
+if __name__ == "__main__":
+    main()
 ###############################################
 ###############################################
 ###############################################
