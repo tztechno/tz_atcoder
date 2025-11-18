6629d451f50884509ac76a3680ed4806ed0ec644
stpeteishii <47345279+tztechno@users.noreply.github.com>
2025-11-18 11:17:45 +0900

    Update 00_my_library.py

--- 00_my_library.py
+++ 00_my_library.py
@@ -30,8 +30,6 @@ import networkx as nx
 from functools import lru_cache
 @lru_cache(maxsize=None)
 
-
-!pip install atcoder-library-python
 from atcoder.segtree import SegTree
 from atcoder.fenwicktree import FenwickTree
 from atcoder.dsu import DSU
